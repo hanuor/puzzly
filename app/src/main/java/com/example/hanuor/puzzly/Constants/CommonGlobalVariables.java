94041caa173ce255a8ff5d5816033631b6506c0c
Shantanu <shanjohri@gmail.com>
2018-07-01 13:25:22 +0530

    Added Room

--- app/src/main/java/com/example/hanuor/puzzly/Constants/CommonGlobalVariables.java
+++ app/src/main/java/com/example/hanuor/puzzly/Constants/CommonGlobalVariables.java
@@ -2,8 +2,11 @@ package com.example.hanuor.puzzly.Constants;
 
 public class CommonGlobalVariables{
     public static String API_MAIN_URL = "http://api.stackexchange.com/2.2/questions?";
-    public static String API_END_URL = "&order=desc&sort=activity&site=puzzling";
+    public static String API_END_URL = "&pagesize=24&order=desc&sort=activity&site=puzzling";
+    public static String API_WITH_BODY_URL = "&filter=withbody";
     public static String DEBUG_LOG = "debugpuzzly";
     public static String ERROR_LOG = "errorpuzzly";
     public static String VERBOSE_LOG = "verbosepuzzly";
+
+
 }
--- app/src/main/java/com/example/hanuor/puzzly/MainActivity.java
+++ app/src/main/java/com/example/hanuor/puzzly/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.hanuor.puzzly;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -11,35 +16,71 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.hanuor.puzzly.Constants.CommonGlobalVariables;
+import com.example.hanuor.puzzly.Constants.Logger;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.puzzledata)
+    TextView puzzleData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+get_data(1);
+    }
 
 
-        // Instantiate the RequestQueue.
+    public void get_data(int pageNumber) {
         RequestQueue queue = Volley.newRequestQueue(this);
-        String page = "page=2";
-        String url = CommonGlobalVariables.API_MAIN_URL + page + CommonGlobalVariables.API_END_URL;
+        String page = "page=" + pageNumber;
+        String url = CommonGlobalVariables.API_MAIN_URL + page + CommonGlobalVariables.API_END_URL + CommonGlobalVariables.API_WITH_BODY_URL;
 
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
+                    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
                     @Override
                     public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        Log.d("Puzzly", response);
+                        Log.e(CommonGlobalVariables.ERROR_LOG, response);
+                        Logger.d(CommonGlobalVariables.DEBUG_LOG, response);
+                        data_response_handler(response);
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.d("error", "That didn't work!");
+                Log.d(CommonGlobalVariables.ERROR_LOG, error.getMessage());
             }
         });
 
-// Add the request to the RequestQueue.
         queue.add(stringRequest);
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void data_response_handler(String response) {
+        Log.d(CommonGlobalVariables.DEBUG_LOG, response);
+
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            Log.v(CommonGlobalVariables.VERBOSE_LOG, jsonObject.toString());
+            JSONArray jsonArray = new JSONArray(jsonObject.getString("items"));
+            Log.e(CommonGlobalVariables.ERROR_LOG, String.valueOf(jsonArray.length()));
+
+            for (int i=0; i<jsonArray.length(); i++) {
+                JSONObject itemObject = jsonArray.getJSONObject(i);
+                puzzleData.setText(Html.fromHtml(itemObject.getString("body"), Html.FROM_HTML_MODE_LEGACY));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 }
